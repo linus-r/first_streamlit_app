73f3786b3aa88b3f2f98b7bcf5f72d74bce4aab7
linus-r <115982412+linus-r@users.noreply.github.com>
2022-10-17 10:54:55 +0200

    Update streamlit_app.py
    Adding first draft of the breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 and Blackberry Oatmeal')
+Streamlit.text('Kale, Spinach & Rocket Smoothie')
