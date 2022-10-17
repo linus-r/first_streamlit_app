ec9eb9ff5dd5d5f78c0ca35dc1f4fc83d9f0bbd6
linus-r <115982412+linus-r@users.noreply.github.com>
2022-10-17 10:55:23 +0200

    Update streamlit_app.py
    typos fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 and Blackberry Oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Omega 3 and Blackberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
